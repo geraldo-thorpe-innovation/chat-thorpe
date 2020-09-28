8e3a46e5bbde3eb85fd258b278fbd87470769857
Geraldo-Thorpe <64435314+geraldo-thorpe-innovation@users.noreply.github.com>
2020-09-28 13:21:40 -0300

    start

---
+++ README.md
@@ -0,0 +1,1 @@
+# chat-miauu-web
---
+++ src/components/TextContainer/TextContainer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './TextContainer.css';
+
+const users = [
+  {
+    id: 1,
+    user: 'Geraldo'
+  },
+  {
+    id: 2,
+    user: 'Geraldo2'
+  },
+  {
+    id: 3,
+    user: 'Geraldo3'
+  },
+  {
+    id: 4,
+    user: 'Geraldo4'
+  },
+  {
+    id: 5,
+    user: 'Geraldo5'
+  },
+  {
+    id: 6,
+    user: 'Geraldo6'
+  },
+  {
+    id: 7,
+    user: 'Geraldo7'
+  },
+]
+
+const TextContainer = () => (
+  <div className="textContainer">
+    {
+      users.map(user => (
+       <a href={`/chat?name=${user.user}&room=${user.id}`} className='room'>
+        <h5>{user.user}</h5>
+       </a>
+      ))
+    }
+  </div>
+);
+
+export default TextContainer;
