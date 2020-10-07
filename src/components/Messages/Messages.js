540fbe72c612721492e1315bd58b82c786d793a4
Geraldo-Thorpe <64435314+geraldo-thorpe-innovation@users.noreply.github.com>
2020-10-07 17:12:37 -0300

    ajustes de recebimedo da mensagem

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -7,22 +7,23 @@ import './Message/Message.css'
 import Message from './Message/Message';
 
 const Messages = ({ messages }) => {
+  console.log(messages);
   return(
     <ScrollToBottom className="messages">
       {
         messages.map(message => (
           <>
           {
-            message.name == 'miauuteam' ? (
+            message.name == 'miauuteam' || message.user == 'miauuteam' ? (
             <div className="messageContainer justifyEnd">
               <div className="messageBox backgroundBlue">
-                <p className="messageText colorWhite">{message.message}</p>
+                <p className="messageText colorWhite">{message.message ? message.message : message.text}</p>
               </div>
             </div>
             ) : (
               <div className="messageContainer justifyStart">
                 <div className="messageBox backgroundLight">
-                  <p className="messageText colorDark">{message.message}</p>
+                  <p className="messageText colorDark">{message.message ? message.message : message.text}</p>
                 </div>
               </div>
             )
